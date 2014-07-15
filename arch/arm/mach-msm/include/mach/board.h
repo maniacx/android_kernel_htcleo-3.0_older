244d16aedbfd308bf033976edfa764c38d3476e9
maniacx <macs18max@gmail.com>
2014-07-15 19:32:16 +0400

    Add more files realted to qsd8k and wince

--- arch/arm/mach-msm/include/mach/board.h
+++ arch/arm/mach-msm/include/mach/board.h
@@ -61,7 +61,7 @@ struct msm_cam_expander_info {
 };
 
 struct msm_camera_device_platform_data {
-	void (*camera_gpio_on) (void);
+	int (*camera_gpio_on) (void);
 	void (*camera_gpio_off)(void);
 	struct msm_camera_io_ext ioext;
 	struct msm_camera_io_clk ioclk;
@@ -564,10 +564,11 @@ void msm_map_copper_io(void);
 void  msm_init_irq(void);
 void msm_copper_init_irq(void);
 
+#ifdef CONFIG_ARCH_MSM7X30
 struct mmc_platform_data;
 int  msm_add_sdcc(unsigned int controller,
 		struct mmc_platform_data *plat);
-
+#endif
 struct msm_usb_host_platform_data;
 int  msm_add_host(unsigned int host,
 		struct msm_usb_host_platform_data *plat);
@@ -671,7 +672,7 @@ int usb_host_detect_register_notifier(struct t_usb_host_status_notifier *);
 static LIST_HEAD(g_lh_usb_host_detect_notifier_list);
 #endif
 /* END: add USB connected notify function */
-
+char *board_serialno(void);
 int board_mfg_mode(void);
 int board_build_flag(void);
 int __init parse_tag_skuid(const struct tag *tags);
