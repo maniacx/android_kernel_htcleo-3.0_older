d62b61f22341b5c5347e502c82aac580d02646bb
maniacx <macs18max@gmail.com>
2014-08-28 12:55:41 +0800

    Fix bluetooth

--- arch/arm/mach-msm/board-htcleo.c
+++ arch/arm/mach-msm/board-htcleo.c
@@ -1633,6 +1633,8 @@ static void __init htcleo_init(void)
 
 #ifdef CONFIG_SERIAL_MSM_HS
 	msm_device_uart_dm1.dev.platform_data = &msm_uart_dm1_pdata;
+	msm_device_uart_dm1.name = "msm_serial_hs";
+	msm_device_uart_dm1.resource[3].end = 6;
 #endif
 
 	config_gpio_table(bt_gpio_table, ARRAY_SIZE(bt_gpio_table));
